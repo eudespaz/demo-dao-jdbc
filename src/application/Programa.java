00bc6966a16d0eaff9e0b26fdfbb52cea929df85
Eudes Paz <eudespaz@live.com>
2019-08-09 23:59:57 -0300

    Class Vendedor

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -1,14 +1,19 @@
 package application;
 
+import java.util.Date;
+
 import modelo.entidades.Departamento;
+import modelo.entidades.Vendedor;
 
 public class Programa {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Departamento departamento = new Departamento(1, "Livros");
-		System.out.println(departamento);
+		Departamento dep = new Departamento(1, "Livros");
+		
+		Vendedor vendedor = new Vendedor(1, "eudes", "eudes@eudes", new Date(), 3000.0, dep);
+		System.out.println(vendedor);
 
 	}
 
--- src/modelo/entidades/Departamento.java
+++ src/modelo/entidades/Departamento.java
@@ -61,7 +61,7 @@ public class Departamento implements Serializable{
 
 	@Override
 	public String toString() {
-		return "Departamento [id=" + id + ", nome=" + nome + "]";
+		return "Departamento id: " + id + ", nome: " + nome;
 	}
 	
 	
