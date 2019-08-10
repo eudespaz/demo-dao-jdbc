34c136760994955785c0061179658b9a90260424
Eudes Paz <eudespaz@live.com>
2019-08-09 23:33:13 -0300

    Class Departamento

---
+++ src/application/Programa.java
@@ -0,0 +1,15 @@
+package application;
+
+import modelo.entidades.Departamento;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Departamento departamento = new Departamento(1, "Livros");
+		System.out.println(departamento);
+
+	}
+
+}
